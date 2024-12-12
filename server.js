c538c48913d05df2d180146cb7b06f3f8ad613b6
Supprit Das <suppritdas@gmail.com>
2024-12-12 10:51:01 +0530

    The server js middle ware is added

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const PORT = 8040;
 const app = express();
 
+//middleware
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
+
+
 app.listen(PORT,()=>{
     console.log(`The Server is Running in http://localhost:${PORT}/`)
 })
